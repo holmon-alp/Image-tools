4f95ebbb62a410f9eb35d07fe8316a33f9db3ad3
Mirjamol Mirislomov <alpholmon@gmail.com>
2024-02-27 21:41:36 +0500

    some changes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Image tools
+
+#### This app is just an amateur creation. The app may contain errors and omissions from various situations.
+Using this program: 
+- apply effects to images;
+- remove background;
+- extract text from image;
+- generate QR code;
+- generate image via Prompt;
+    
+The program was prepared by **Mirjamol Mirislomov** on 02.27.2024.
+    
+Source code of the program on Github: [Link](https://github.com/holmon-alp/Image-tools.git)
--- app.py
+++ app.py
@@ -143,8 +143,8 @@ if file:
     buttons()
     
 st.markdown(
-    """#### This app is just an amateur creation. The app may contain errors and omissions from various situations.
-
+    """
+    #### This app is just an amateur creation. The app may contain errors and omissions from various situations.
     Using this program: 
     - apply effects to images;
     - remove background;
@@ -154,5 +154,6 @@ st.markdown(
     
     The program was prepared by **Mirjamol Mirislomov** on 02.27.2024.
     
-    Source code of the program on Github: [Link](https://github.com/holmon-alp/Image-tools.git)"""
+    Source code of the program on Github: [Link](https://github.com/holmon-alp/Image-tools.git)
+    """
 )
