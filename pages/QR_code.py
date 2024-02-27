37da767bbfdaa9518742217ed645e57bb5fb1a97
Mirjamol Mirislomov <alpholmon@gmail.com>
2024-02-27 20:41:00 +0500

    some changes

--- pages/QR_code.py
+++ pages/QR_code.py
@@ -9,10 +9,11 @@ content = st.text_input("Enter a text you want to generate QR", placeholder="Ent
 
 if len(content) > 0:
     qr = segno.make_qr(content)
-    qr.save("temp_qr.png")
+    qr.save("temp_qr.png", scale=12)
     # img = Image.open("temp_qr.png").resize((424,424))
     # img = np.array(img)
     st.image("temp_qr.png", caption="Generated QR")
+    st.download_button("Download", "temp_qr.png", file_name="qr_code.png")
 
  # Paste QR code onto the background at position (50,  50)
 # qr.to_artistic(background='/home/holmon/Downloads/hazal.jpg', target='qr_with_background.png', scale=5)
