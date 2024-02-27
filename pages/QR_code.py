826fb0aade8314c44f0adfff5805a9fcf214e31d
Mirjamol Mirislomov <alpholmon@gmail.com>
2024-02-27 21:03:30 +0500

    some changes

--- pages/QR_code.py
+++ pages/QR_code.py
@@ -6,19 +6,19 @@ from app import to_bytes
 
 st.title(" Create a QR code as you like!!!")
 content = st.text_input("Enter a text you want to generate QR", placeholder="Enter text...")
-def show_(path: str):
+def show_(path: str, key:str):
     st.image(path, caption="Generated QR")
     img = Image.open(path)
-    st.download_button("Download", to_bytes(img, "png"), file_name="qr_code.png")
+    st.download_button("Download", to_bytes(img, "png"), file_name="qr_code.png", key=key)
 
 if len(content) > 0:
     qr = segno.make_qr(content)
     qr.save("temp_qr.png", scale=12)
-    show_("temp_qr.png")
+    show_("temp_qr.png", "1")
 
     st.markdown("### You can more creative your QR code")
     back = st.file_uploader("Upload image for background", type=["png", "jpg", "gif"])
     if back:
         qr.to_artistic(background=back, target='qr_with_background.png', scale=12)
-        show_("qr_with_background.png")
+        show_("qr_with_background.png", "2")
  
