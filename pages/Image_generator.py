13dcf43c5b033498eafb1b7feacfe4bf9226f7c2
Mirjamol Mirislomov <96412090+holmon-alp@users.noreply.github.com>
2024-03-03 12:51:50 +0500

    Update Image_generator.py

--- pages/Image_generator.py
+++ pages/Image_generator.py
@@ -1,50 +1,59 @@
 import streamlit as st
- 
+from app import to_bytes
 # Uncomment the following code to use the image generator
  
-# import torch
-# from diffusers import StableDiffusionPipeline
+import torch
+from diffusers import StableDiffusionPipeline
 
-# image_model_id = "stabilityai/stable-diffusion-2"
-# # device = "cuda"
-# device = torch.device("cuda")
-# image_gen_steps = 35
-# generator = torch.Generator(device).manual_seed(42)
-# guidance_scale = 9
-# image_size = (432, 432)
 
-# image_gen_model = StableDiffusionPipeline.from_pretrained(
-#     image_model_id, torch_dtype=torch.float16,
-#     variant="fp16" #, use_auth_token="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", guidance_scale=9
-# )
+class ImgGen:
 
-# image_gen_model = image_gen_model.to(device)
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        generator = torch.Generator(device).manual_seed(42)
+    else:
+        device = torch.device("cpu")
+        generator = torch.Generator(device).manual_seed(42)
 
-# def generate_image(prompt):
-#   image = image_gen_model(
-#       prompt, num_interface_steps=image_gen_steps,
-#       generator=generator, guide_scale=guidance_scale,
-#   ).images[0]
-#   image = image.resize(image_size)
-#   return image
+    image_model_id = "stabilityai/stable-diffusion-2"
+    # device = "cuda"
 
-# st.set_page_config(
-#     page_title="Image generator",
-#     page_icon="💡",
-# )
-# st.title("Text to image generator")
+    image_gen_steps = 35
+    guidance_scale = 9
+    image_size = (500, 500)
 
-# prompt = st.text_input("Enter prompt to generate image", placeholder="Enter prompt...")
-# if len(prompt) > 0:
-#     with st.spinner('Wait a moment...'):
-#         img = generate_image(prompt)
-#     st.image(img)
-#     st.download_button(
-#             "Download", 
-#             to_bytes(img, "PNG"), 
-#             file_name="image.png",
-#             type="primary"
-#         )
+    image_gen_model = StableDiffusionPipeline.from_pretrained(
+        image_model_id, #torch_dtype=torch.float16,
+        variant="fp16", use_auth_token="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", guidance_scale=9
+    )
+
+    image_gen_model = image_gen_model.to(device)
+
+    def generate_image(prompt):
+        image = ImgGen.image_gen_model(
+            prompt, num_interface_steps=ImgGen.image_gen_steps,
+            generator=ImgGen.generator, guide_scale=ImgGen.guidance_scale,
+        ).images[0]
+        image = image.resize(ImgGen.image_size)
+        return image
+
+st.set_page_config(
+    page_title="Image generator",
+    page_icon="💡",
+)
+st.title("Text to image generator")
+
+prompt = st.text_input("Enter prompt to generate image", placeholder="Enter prompt...")
+if len(prompt) > 0:
+    with st.spinner('Wait a moment...'):
+        img = ImgGen.generate_image(prompt)
+    st.image(img)
+    st.download_button(
+            "Download", 
+            to_bytes(img, "PNG"), 
+            file_name="image.png",
+            type="primary"
+        )
 
 st.warning("This feature doesn't work here because GPU doesn't support it 😥")
 st.markdown(
